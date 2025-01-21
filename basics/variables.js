8cad6352eda606a4b240741e067520c1fe342747
Aman Jha <122011827+Amanj-0349@users.noreply.github.com>
2025-01-21 10:55:09 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,16 @@
+const accountid = 1225
+let email  = "amanjha@gmail.com"
+var password = 12345
+accountCity = "newDelhi"
+let accountState ;
+console.log(accountid)
+console.table([email,password,accountCity,accountState])
+
+/* prefer not to use var 
+bcz of issue in functional scope and block scope */
+
+/* if i just normally declare the variable without defining its value js will take it as 
+"undefined"*/
+
+
+
