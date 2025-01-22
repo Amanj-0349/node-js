e08e8a07ffd13cbad1a04d9ae59eee081be6073b
Aman Jha <122011827+Amanj-0349@users.noreply.github.com>
2025-01-22 09:51:36 +0000

    variables

--- basics/variables.js
+++ basics/variables.js
@@ -6,7 +6,7 @@ let accountState ;
 console.log(accountid)
 console.table([email,password,accountCity,accountState])
 
-/* prefer not to use var 
+/* prefer not to use- var 
 bcz of issue in functional scope and block scope */
 
 /* if i just normally declare the variable without defining its value js will take it as 
