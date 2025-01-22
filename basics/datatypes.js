8688c59ac71f7486e381ef9bb54843f494a8bd7c
Aman Jha <122011827+Amanj-0349@users.noreply.github.com>
2025-01-22 07:52:59 +0000

    datatype

---
+++ basics/datatypes.js
@@ -0,0 +1,38 @@
+/* java script has 8 data types 
+1.number 
+2.string
+3.null
+4.bigint
+5.undefined 
+6.object
+7.boolean 
+8.symbol
+*/
+
+// number 
+console.log(typeof 3+3)
+
+// string 
+console.log(typeof "Aman jha");
+
+// null 
+console.log(typeof null);
+// typeof null is object 
+
+// undefined 
+console.log(typeof undefined);
+//typeof undefined is undefined 
+
+//boolean 
+console.log(typeof true);
+// its type is boolean
+
+// object 
+console.log(typeof object);
+// type of object is undefined 
+
+// use strict - it is used to use the newer version of js 
+
+
+
+
