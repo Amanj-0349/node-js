c773e9edda15e87703df06b4e42104b0018a6b20
Aman Jha <122011827+Amanj-0349@users.noreply.github.com>
2025-01-25 08:15:39 +0000

    functions

---
+++ basics2/functions.js
@@ -0,0 +1,61 @@
+function name(){
+    console.log('Aman jha');
+    
+}
+
+name()
+
+// lets make a normal function for the addition of two number 
+
+function add(number1,number2){
+    return number1+number2
+}
+
+// const res = add(3,5)
+// console.log(res);
+
+// basically return is used to end the function or to return something from the given function 
+
+/**********one more function  */
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("PLease enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"))
+// console.log(loginUserMessage("hitesh"))
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 400, 500, 2000))
+
+const user = {
+    username: "hitesh",
+    prices: 199
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
